78157702e301acab5f0cb3ebf236bbcebddd1104
David Bründel <davidbrundel@MacBook-Pro-von-David-2.local>
2022-10-10 22:12:09 +0200

    Jetstream installiert

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+ 
 export default defineConfig({
     plugins: [
         laravel([
@@ -9,3 +9,5 @@ export default defineConfig({
         ]),
     ],
 });
+
+
